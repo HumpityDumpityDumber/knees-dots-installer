4d5b44f35d31380b494a08fb85537df8e029420a
knee <knee@raurutuchr.ink>
2025-07-11 23:58:56 -0400

    Refactor dotfiles and configurations
    - Removed quickCss.css as it is no longer needed.
    - Updated settings.json to remove the old toggleVideoBind and add ToggleVideoBind with the same settings.
    - Deleted fish_variables file to clean up unused configurations.
    - Modified mako config for updated color scheme.
    - Enhanced niri configuration with new layout settings and window rules.
    - Changed permissions for various niri scripts to make them executable.
    - Updated swaylock configuration for improved blur and vignette effects.
    - Adjusted waybar configuration for better spacing and module management.
    - Refined waybar styles for a more cohesive look.
    - Expanded README.md to provide clearer installation instructions and warnings.
    - Improved install.fish script for better user interaction and package management.
    - Restructured packages.json to categorize core, extra, and gaming packages for easier management.

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-This is my personal dotfile installer - probably don't use unless you are willing to make some changes to some config files (also it just installs a ton of packages)
+# Knee's Dotfiles Installer
+
+⚠️ **WARNING: This is a personal dotfiles installer!** ⚠️
+
+This repository contains my personal dotfiles and configuration setup. It is tailored specifically for my workflow, preferences, and system. **Use at your own risk** - installing these dotfiles may overwrite your existing configurations. ***Below you can read about configurations you may want to change, since they are tailored to me.***
+
+## Overview
+
+This installer gives you a niri installation with rofi as the launcher, mako as the notif daemon, mostly gnome system apps, and generally a gruvbox theme with some exceptions.
+
+## Prerequisites
+
+before downloading and using the installer you will need
+
+- git
+- fish
+
+## Installation
+
+to install use
+
+```
+https://github.com/HumpityDumpityDumber/knees-dots-installer.git
+```
+**make sure /home/{your_user} is the current working directory (this can be checked by running `pwd`)
+
+## What's Included
+
+These dotfiles include
+
+- equibop
+- fastfetch
+- fish (gruv colors, and an opening animation using sttt configured to work with kitty)
+- kitty (gruv theme)
+- mako (simple blue gruv theme)
+- niri (mostly gradients, easy to pop out of this repo with no harm other than dependencies. these can be found within the keybinds and startup if you wish to download/replace them.)
+- rofi (gruv theme with credits within the file)
+- swayidle (simple locking with swaylock)
+- swaylock-effects (gruv themed)
+- waybar (semi gruv themed blue waybar)
+- xdg-desktop-portal (this is just to make sure you are using xdg-desktop-portal-gnome)
+- starship (tree)
+
+You also get Google Sans Flex edited to just be rounded patched by me and [@twigform](https://github.com/twigform)!
+
+## !! What to change !!
+
+There are many places where you may want to change the configuration where it is specific to my system. These will be listed below:
+
+## Troubleshooting
+
+There might be problems with the **xwayland display** and **wayland display** so try setting those to `WAYLAND_DISPLAY "wayland-1"` and `    DISPLAY ":0"` respectively in the niri config (`config.kdl` in the `env{}` section)
+
+## License
+
+My dotfiles are under the unlicense (yeah just do whatever)
--- install.fish
+++ install.fish
@@ -20,10 +20,11 @@ echo (set_color cyan)"
 ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║         
 ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝         
 "(set_color normal)
+sleep 1
 echo (set_color green)"Welcome to Knee's Dotfiles Installer!"(set_color normal)
 echo (set_color green)"========================================"(set_color normal)
 echo ""
-sleep 2
+sleep 1
 
 # ask for user permission func
 function read_confirm
@@ -41,41 +42,51 @@ end
 
 echo (set_color blue)"This script will install various packages and configurations for your Arch Linux system."(set_color normal)
 echo (set_color blue)"It will also set up a GRUB theme and an audio visualizer bar."(set_color normal)
+sleep 1
 # ask for user permission
 if read_confirm
     echo (set_color green)"Continuing with the installation..."(set_color normal)
+    sleep 1
 else
     echo (set_color red)"Installation aborted. Exiting..."(set_color normal)
+    sleep 1
     exit 0
 end
 
 # check if user is on Arch Linux
 echo (set_color yellow)"Checking if you're on Arch Linux..."(set_color normal)
+sleep 1
 set os (grep '^NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
 if string match -q '*Arch*' $os
     echo (set_color green)"You're on Arch Linux! Continuing with the installation..."(set_color normal)
+    sleep 1
 else
     echo (set_color red)"This is not Arch Linux, installation aborted. Please run this script on Arch Linux."(set_color normal)
+    sleep 1
     exit 1
 end
 
 # configure pacman
 echo (set_color yellow)"Configuring pacman..."(set_color normal)
+sleep 1
 sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 sudo sed -i 's/^#ILoveCandy/ILoveCandy/' /etc/pacman.conf
 sudo sed -i '/^#\[multilib\]/,/^#Include/ s/^#//' /etc/pacman.conf
 
 # update package databases
 echo (set_color yellow)"Updating package databases..."(set_color normal)
+sleep 1
 sudo pacman -Sy
 
 # install base-devel if not already installed
 echo (set_color yellow)"Installing base-devel..."(set_color normal)
+sleep 1
 sudo pacman -S --needed --noconfirm base-devel git
 
 # Install yay if not already installed
 if not type -q yay
     echo (set_color yellow)"Installing yay AUR helper..."(set_color normal)
+    sleep 1
     git clone https://aur.archlinux.org/yay.git
     pushd yay
     makepkg -si --noconfirm
@@ -83,58 +94,121 @@ if not type -q yay
     rm -rf yay
 else
     echo (set_color green)"yay is already installed, skipping..."(set_color normal)
+    sleep 1
 end
 
 # install packages from packages.json
-echo (set_color yellow)"Installing packages from packages.json..."(set_color normal)
+echo (set_color yellow)"Installing core packages from packages.json..."(set_color normal)
+sleep 1
 sudo pacman -S --noconfirm jq
-set packages (jq -r '.[]' packages.json)
-for package in $packages
+
+# Install core packages
+set core_packages (jq -r '.core[]' packages.json)
+for package in $core_packages
     echo (set_color cyan)"Installing $package..."(set_color normal)
+    sleep 1
     yay -S --noconfirm --needed $package
     if test $status -ne 0
         echo (set_color red)"Failed to install $package, continuing..."(set_color normal)
+        sleep 1
     end
 end
 
-# install flatpaks
-echo (set_color yellow)"Installing Flatpaks..."(set_color normal)
-flatpak install --assumeyes flathub \
-    com.obsproject.Studio \
-    com.jeffser.Alpaca \
-    com.github.marhkb.Pods \
-    io.mrarm.mcpelauncher \
+# Ask for extra packages
+set install_extra false
+echo (set_color blue)"Extra packages include applications like Spotify, Obsidian, GIMP, Blender, and more."(set_color normal)
+if read_confirm
+    echo (set_color green)"Installing extra packages..."(set_color normal)
+    set install_extra true
+    set extra_packages (jq -r '.extra[]' packages.json)
+    for package in $extra_packages
+        echo (set_color cyan)"Installing $package..."(set_color normal)
+        sleep 1
+        yay -S --noconfirm --needed $package
+        if test $status -ne 0
+            echo (set_color red)"Failed to install $package, continuing..."(set_color normal)
+            sleep 1
+        end
+    end
+else
+    echo (set_color yellow)"Skipping extra packages..."(set_color normal)
+    sleep 1
+end
 
-# install spicetify
-echo (set_color yellow)"Installing Spicetify CLI..."(set_color normal)
-curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
+# Ask for gaming packages
+echo (set_color blue)"Gaming packages include Steam, Lutris, Wine, and various gaming libraries."(set_color normal)
+if read_confirm
+    echo (set_color green)"Installing gaming packages..."(set_color normal)
+    set gaming_packages (jq -r '.gaming[]' packages.json)
+    for package in $gaming_packages
+        echo (set_color cyan)"Installing $package..."(set_color normal)
+        sleep 1
+        yay -S --noconfirm --needed $package
+        if test $status -ne 0
+            echo (set_color red)"Failed to install $package, continuing..."(set_color normal)
+            sleep 1
+        end
+    end
+else
+    echo (set_color yellow)"Skipping gaming packages..."(set_color normal)
+    sleep 1
+end
 
-# set permissions for spicetify
-echo (set_color yellow)"Setting permissions for Spicetify..."(set_color normal)
-sudo chmod a+wr /opt/spotify
-sudo chmod a+wr /opt/spotify/Apps -R
+# Ask for flatpaks
+echo (set_color blue)"Flatpaks include OBS Studio, Alpaca, Pods, and Minecraft Launcher."(set_color normal)
+if read_confirm
+    echo (set_color yellow)"Installing Flatpaks..."(set_color normal)
+    sleep 1
+    flatpak install --assumeyes flathub \
+        com.obsproject.Studio \
+        com.jeffser.Alpaca \
+        com.github.marhkb.Pods \
+        io.mrarm.mcpelauncher \
+else
+    echo (set_color yellow)"Skipping Flatpaks..."(set_color normal)
+    sleep 1
+end
 
-# patch spotify
-echo (set_color yellow)"Patching Spotify with Spicetify..."(set_color normal)
-spicetify backup apply
+# Install and configure spicetify only if extra packages were installed
+if test $install_extra = true
+    # install spicetify
+    echo (set_color yellow)"Installing Spicetify CLI..."(set_color normal)
+    sleep 1
+    curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
+
+    # set permissions for spicetify
+    echo (set_color yellow)"Setting permissions for Spicetify..."(set_color normal)
+    sleep 1
+    sudo chmod a+wr /opt/spotify
+    sudo chmod a+wr /opt/spotify/Apps -R
+
+    # patch spotify
+    echo (set_color yellow)"Patching Spotify with Spicetify..."(set_color normal)
+    sleep 1
+    spicetify backup apply
+end
 
 # fixing micro
 echo (set_color yellow)"Fixing Micro editor..."(set_color normal)
+sleep 1
 sudo ln -sf /usr/bin/kitty /usr/local/bin/xterm
 
 # download grub theme
 echo (set_color yellow)"Downloading GRUB theme..."(set_color normal)
+sleep 1
 sudo git clone --depth 1 https://github.com/13atm01/GRUB-Theme.git "/tmp/GRUB-Theme/"
 sudo mv "/tmp/GRUB-Theme/Inoue Takina" /tmp/GRUB-Theme/Inoue-Takina
 
 # install grub theme
 echo (set_color yellow)"Installing GRUB theme..."(set_color normal)
+sleep 1
 cd "/tmp/GRUB-Theme/Inoue-Takina"
 sudo chmod +x install.sh
 sudo ./install.sh
 
 # configure plymouth
 echo (set_color yellow)"Configuring Plymouth..."(set_color normal)
+sleep 1
 
 # Insert ShowDelay=0 after [Daemon] in the Plymouth config
 sudo sed -i '/^\[Daemon\]/a ShowDelay=0' /etc/plymouth/plymouthd.conf
@@ -154,44 +228,53 @@ sudo plymouth-set-default-theme -R bgrt
 
 # update grub config
 echo (set_color yellow)"Updating GRUB configuration..."(set_color normal)
+sleep 1
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # download audio visualizer bar
 echo (set_color yellow)"Downloading Knee's audio visualizer bar..."(set_color normal)
+sleep 1
 sudo git clone --depth 1 https://github.com/HumpityDumpityDumber/python-bar-rewrite.git "/opt/python-bar-rewrite/"
 
 # install audio visualizer bar
 echo (set_color yellow)"Installing Knee's audio visualizer bar..."(set_color normal)
+sleep 1
 cd /opt/python-bar-rewrite/
 sudo meson setup build --wipe
 sudo meson install -C build
 
 # copy configs to config folder (niri, equibop, etc.)
 echo (set_color yellow)"Copying configuration files..."(set_color normal)
+sleep 1
 mkdir -p ~/.config
 cp -r "$SCRIPT_DIR/.config/"* $HOME/.config/
 
 # set perms for niri scripts
 echo (set_color yellow)"Setting permissions for niri scripts..."(set_color normal)
+sleep 1
 chmod +x $HOME/.config/niri/scripts/*
 
 # enable and set GDM as display manager
 echo (set_color yellow)"Enabling GDM display manager..."(set_color normal)
+sleep 1
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
 
 # set fish as default shell
 echo (set_color yellow)"Setting Fish as the default shell..."(set_color normal)
+sleep 1
 chsh -s (which fish)
 
 # copy fonts into font folder
 echo (set_color yellow)"Copying fonts to the font folder..."(set_color normal)
+sleep 1
 mkdir -p $HOME/.local/share/fonts
 cp -r "$SCRIPT_DIR/fonts/"* $HOME/.local/share/fonts/
 fc-cache -fv
 
 # fix gtk buttons
-echo (set_color yellow)"Fixing GTK configuration..."(set_color normal)    
+echo (set_color yellow)"Fixing GTK configuration..."(set_color normal)
+sleep 1
 gsettings set org.gnome.desktop.wm.preferences button-layout :
 gsettings set org.gnome.desktop.interface icon-theme 'Pop'
 gsettings set org.gnome.desktop.interface font-name 'Google Sans Rounded Bold 11'
@@ -200,24 +283,36 @@ gsettings set org.gnome.desktop.interface cursor-theme 'gruvterial'
 
 # clone gruvbox wallpapers
 echo (set_color yellow)"Cloning Gruvbox wallpapers..."(set_color normal)
+sleep 1
 git clone --depth 1 https://github.com/AngelJumbo/gruvbox-wallpapers.git $HOME/Pictures/gruvpapers
 
 # clean up
 echo (set_color yellow)"Cleaning up..."(set_color normal)
+sleep 1
 sudo rm -rf /opt/GRUB-Theme/
 sudo rm -rf /opt/python-bar-rewrite/
 yay -Yc --noconfirm
 sudo pacman -Sc --noconfirm
 
 echo (set_color green)"Installation completed successfully!"(set_color normal)
+sleep 1
 
-echo (set_color blue)"run 'nerdfonts-installer' to install Nerd Fonts. (it isnt run automatically because it takes a long time to install)"(set_color normal)
+echo (set_color blue)"do you want to run 'nerdfonts-installer'? Be aware that installing all nerd fonts takes a while!"(set_color normal)
+sleep 1
+if read_confirm
+    echo (set_color green)"Running 'nerdfonts-installer'..."(set_color normal)
+    sleep 1
+    nerdfonts-installer
+end
 
 echo (set_color blue)"Rebooting your system is recommended to apply all changes successfully."(set_color normal)
+sleep 1
 
 if read_confirm   
     echo (set_color green)"Rebooting your system..."(set_color normal)
+    sleep 1
     sudo reboot
 else
     echo (set_color yellow)"You can reboot later to apply the changes."(set_color normal)
+    sleep 1
 end
